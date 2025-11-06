4186692d15dbffca0844c76fb0439b625718b46b
Krukowskii <95411151+Krukowskii@users.noreply.github.com>
2025-11-06 23:55:34 +0100

    Update vector.h

--- vector/vector.h
+++ vector/vector.h
@@ -2,9 +2,6 @@
 #define VECTOR_H
 
 #include <cstddef>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 template<typename T>
 class vector{
@@ -12,21 +9,25 @@ class vector{
     T* arr;
     std::size_t count;
     std::size_t capacity;
+
     public:
     vector();
     ~vector();
 
+    //helpers
+    void isFull();
+    void isToBig();
+    
     void push_back(const T& value);
     void pop_back();
     void erase(const std::size_t index);
-    bool empty();
-    void shrink_to_fit();
+    void shrinkToFit();
 
-    T& operator[](std::size_t index){return arr[index];}
+    T& operator[](std::size_t index) {return arr[index];}
     std::size_t size() const {return count;}
+    bool empty() const {return count == 0;}
     T* begin() const {return arr;}
     T* end() const {return arr + count;}
 };
 
 #endif
-
